22647a7dbfeb3997934871348ff3fb3368d14250
Fardosafara <firdausgedi2@gmail.com>
2024-03-26 17:53:24 +0300

    index.js

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((sentence)=>{
+    //we are splitting each sentence using the space into  words
+    const words=sentence.split(" ")
+    console.log(words)
+    const titlecaseWord= words.map((word)=>{
+        console.log(word)
+       return word.charAt(0).toUpperCase()+ word.slice(1)
+    })
+    const result=titlecaseWord.join(" ")
+    
+    return result
+ })
+  
 }
